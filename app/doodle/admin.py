dde23e8bedc5614206ce9df09d17020ac15a4b09
Lancelot <bincha.1997@gmail.com>
2019-11-11 23:33:26 +0300

    add admin doodle models

--- app/doodle/admin.py
+++ app/doodle/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from doodle.models import Post, Comment, Tag
 
-# Register your models here.
+
+@admin.register(Post)
+class AdminPost(admin.ModelAdmin):
+
+    list_display = ('author', 'text',)
+
+
+@admin.register(Comment)
+class AdminComment(admin.ModelAdmin):
+
+    list_display = ('author', 'text',)
+
+@admin.register(Tag)
+class AdmonTag(admin.ModelAdmin):
+
+    list_display = ('title',)
